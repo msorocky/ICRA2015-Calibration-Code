f5042d95b998847b5fac74e70735001237b34359
justin <jltomasi11@gmail.com>
2019-04-16 15:38:49 -0400

    updated plot_trajectories.m

--- plot_trajectories.m
+++ plot_trajectories.m
@@ -3,8 +3,45 @@ clear;
 close all;
 
 % Load the trajectory data
+% load('zeusNavData.mat');
+% load('zeusVelData.mat');
+
+tformIdx = 1;
+
+%% process velodyne
+load('zeusVelData.mat');          % data obtained from genKittiVel/genKittiVel.m
+sensorData{tformIdx,1} = velData; % This would be line 2 in the paper's Algorithm 1 
+tformIdx = tformIdx + 1;
+
+range = 1:size(velData.T_S1_Sk,1);
+
+%% process nav
 load('zeusNavData.mat');
-load('zeusVelData.mat');
+sensorData{tformIdx,1} = navData;
+tformIdx = tformIdx + 1;
+
+%% Interpolate the trajectories
+
+for i = 1:length(sensorData)
+    if(i > 1)
+        % This interpolates transforms from sensor A to match times of
+        % corresponding sensor B transform      
+        % matchTforms(sensorA, sensorB, range, addError) -- range is the
+        % timesteps you care about, addError bool for adding noise to each
+        % transformation
+        sensorData{i} = matchTforms(sensorData{i}, sensorData{1},range, false);
+    else
+        % T_Skm1_Sk is the transformation from the frame of the sensor at 
+        % timestep k-1 to its frame at timestep k; T_Cov_Skm1_Sk is its
+        % associated covariance
+        % T_S1_Sk is the transformation from t = 1 to t = k
+        sensorData{i}.T_Skm1_Sk = sensorData{i}.T_Skm1_Sk(range,:);
+        sensorData{i}.T_S1_Sk = sensorData{i}.T_S1_Sk(range,:);
+        sensorData{i}.T_Cov_Skm1_Sk = sensorData{i}.T_Cov_Skm1_Sk(range,:);
+        sensorData{i}.time = sensorData{i}.time(range,:);
+        sensorData{i}.files = sensorData{i}.files(range,:);
+    end
+end
 
 % Rotate the data
 C = [0, sin(-pi/2), 0; -sin(-pi/2), 0, 0; 0, 0, 1];
@@ -12,7 +49,7 @@ t = [0;0;0];
 T = [[C,t]; 0 0 0 1];
 
 % Augment the velodyne data
-velTraj = velData.T_S1_Sk;
+velTraj = sensorData{1}.T_S1_Sk;
 vel_x_y_z = zeros(size(velTraj,1),3);
 vel_th1 = zeros(size(velTraj,1),3);
 vel_th2 = zeros(size(velTraj,1),3);
@@ -34,8 +71,8 @@ for i = 1:size(velTraj,1)
 end
 
 % Organize the data
-navTraj = navData.T_S1_Sk(1:11:end,:);
-velTraj = velData.T_S1_Sk;
+navTraj = sensorData{2}.T_S1_Sk;
+velTraj = sensorData{1}.T_S1_Sk;
 x_nav = -navTraj(:,1);
 y_nav = navTraj(:,2);
 z_nav = navTraj(:,3);
